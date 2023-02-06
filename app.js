6b0173b74b955f043e40c3a550655ca6e6ac7db0
simonkno <123570035+simonkno@users.noreply.github.com>
2023-02-06 12:06:35 -0500

    variables

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "carros",
+      img: "images/carros1.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros2.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros3.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros4.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros5.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros6.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros1.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros2.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros3.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros4.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros5.png"
+    },
+    {
+      name: "carros",
+      img: "images/carros6.png"
+    }
+  ];
+});

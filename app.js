0f5e9b4673bddfa938e0097bb43cfce80732b34b
simonkno <123570035+simonkno@users.noreply.github.com>
2023-02-13 11:47:42 -0500

    nuevocopia

--- app.js
+++ app.js
@@ -49,4 +49,23 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/6.jpg"
     }
   ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  //----------------- lecture_03 ----------------------------------//
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+
+      cuadricula.appendChild(carta);
+    }
+  }
 });
